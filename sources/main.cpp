8c7da5207e02b7abc6053b77d88a340685f0fbf6
tom10987 <tom10987@gmail.com>
2016-07-20 12:41:35 +0900

    ライブラリ導入、GLFW 動作確認完了

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,29 @@
 ﻿
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#pragma comment (lib, "opengl32")
+#if _DEBUG
+#pragma comment (lib, "glfw3d")
+#else
+#pragma comment (lib, "glfw3")
+#endif
+
 int main()
 {
+  if (!glfwInit()) { return 0; }
+
+  auto window = glfwCreateWindow(800, 600, "game framework", nullptr, nullptr);
+  if (!window) { glfwTerminate(); return 0; }
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 }
